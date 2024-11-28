8ecade8f5ef50f8f1465c9fb1851c2e050f74b5b
MINH <MINH@DESKTOP-AV5O43N>
2024-11-28 22:06:56 +0700

    Add project files.

---
+++ OpenGL_Demo/main.cpp
@@ -0,0 +1,382 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <assert.h>
+
+#include <glew.h>
+#include <freeglut.h>
+
+#include <Vector_Matrix.h>
+
+
+using namespace std;
+
+typedef struct
+{
+	float XYZW[4];
+	float RGBA[4];
+} Vertex;
+
+Vertex Vertices[] = {
+	// v0-v1-v2 (front)
+	{ { 0.5f,	 0.5f,	0.5f,	1.0f },{ 1.0f,	0.0f,	0.0f,	1.0f } },
+	{ { -0.5f,	 0.5f,	0.5f,	1.0f },{ 1.0f,	0.0f,	0.0f,	1.0f } },
+	{ { -0.5f,	-0.5f,	0.5f,	1.0f },{ 1.0f,	0.0f,	0.0f,	1.0f } },
+	// v2-v3-v0
+	{ { -0.5f,	-0.5f,	0.5f,	1.0f },{ 1.0f,	0.0f,	0.0f,	1.0f } },
+	{ { 0.5f,	-0.5f,	0.5f,	1.0f },{ 1.0f,	0.0f,	0.0f,	1.0f } },
+	{ { 0.5f,	 0.5f,	0.5f,	1.0f },{ 1.0f,	0.0f,	0.0f,	1.0f } },
+
+	// v0-v3-v4 (right)
+	{ { 0.5f,	 0.5f,	 0.5f,	1.0f },{ 0.0f,	1.0f,	0.0f,	1.0f } },
+	{ { 0.5f,	-0.5f,   0.5f,	1.0f },{ 0.0f,	1.0f,	0.0f,	1.0f } },
+	{ { 0.5f,	-0.5f,  -0.5f,	1.0f },{ 0.0f,	1.0f,	0.0f,	1.0f } },
+	// v4-v5-v0
+	{ { 0.5f,	-0.5f,	-0.5f,	1.0f },{ 0.0f,	1.0f,	0.0f,	1.0f } },
+	{ { 0.5f,	 0.5f,	-0.5f,	1.0f },{ 0.0f,	1.0f,	0.0f,	1.0f } },
+	{ { 0.5f,	 0.5f,   0.5f,	1.0f },{ 0.0f,	1.0f,	0.0f,	1.0f } },
+
+	// v0-v5-v6 (top)
+	{ { 0.5f,	 0.5f,	 0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { 0.5f,	0.5f,	-0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { -0.5f,	0.5f,	 -0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	// v6-v1-v0
+	{ { -0.5f,	 0.5f,	 -0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { -0.5f,	0.5f,	0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { 0.5f,	0.5f,	 0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+
+	// v1-v6-v7 (left)
+	{ { -0.5f,	 0.5f,	 0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { -0.5f,	0.5f,	-0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { -0.5f,	-0.5f,	 -0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	// v7-v2-v1
+	{ { -0.5f,	 -0.5f,	 -0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { -0.5f,	-0.5f,	0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+	{ { -0.5f,	0.5f,	 0.5f,	1.0f },{ 0.0f,	0.0f,	1.0f,	1.0f } },
+
+	// v7-v4-v3 (bottom)
+	{ { -0.5f,	 -0.5f,	 -0.5f,	1.0f },{ 0.1f,	0.2f,	0.3f,	1.0f } },
+	{ { 0.5f,	-0.5f,	-0.5f,	1.0f },{ 0.1f,	0.2f,	0.3f,	1.0f } },
+	{ { 0.5f,	-0.5f,	 0.5f,	1.0f },{ 0.1f,	0.2f,	0.3f,	1.0f } },
+	// v3-v2-v7
+	{ { 0.5f,	 -0.5f,	 0.5f,	1.0f },{ 0.1f,	0.2f,	0.3f,	1.0f } },
+	{ { -0.5f,	-0.5f,	0.5f,	1.0f },{ 0.1f,	0.2f,	0.3f,	1.0f } },
+	{ { -0.5f,	-0.5f,	 -0.5f,	1.0f },{ 0.1f,	0.2f,	0.3f,	1.0f } },
+
+	// v4-v7-v6 (back)
+	{ { 0.5f,	 -0.5f,	 -0.5f,	1.0f },{ 0.4f,	0.5f,	0.6f,	1.0f } },
+	{ { -0.5f,	-0.5f,	-0.5f,	1.0f },{ 0.4f,	0.5f,	0.6f,	1.0f } },
+	{ { -0.5f,	0.5f,	 -0.5f,	1.0f },{ 0.4f,	0.5f,	0.6f,	1.0f } },
+	// v6-v5-v4
+	{ { -0.5f,	 0.5f,	 -0.5f,	1.0f },{ 0.4f,	0.5f,	0.6f,	1.0f } },
+	{ { 0.5f,	0.5f,	 -0.5f,	1.0f },{ 0.4f,	0.5f,	0.6f,	1.0f } },
+	{ { 0.5f,	-0.5f,	 -0.5f,	1.0f },{ 0.4f,	0.5f,	0.6f,	1.0f } }
+};
+
+const size_t BufferSize = sizeof(Vertices);
+const size_t VertexSize = sizeof(Vertices[0]);
+const size_t RgbOffset = sizeof(Vertices[0].XYZW);
+
+class MatrixStack {
+	int index;
+	int size;
+	mat4* matrices;
+public:
+	MatrixStack(int numMatrices = 32) : index(0), size(numMatrices) {
+		matrices = new mat4[numMatrices];
+	}
+	~MatrixStack() {
+		delete[]matrices;
+	}
+	mat4& push(const  mat4& m) {
+		assert(index + 1 < size);
+		matrices[index++] = m;
+		return matrices[index];
+	}
+	mat4& pop(void) {
+		assert(index - 1 < size);
+		index--;
+		return matrices[index];
+	}
+};
+MatrixStack mvstack;
+int CurrentWidth = 700,
+CurrentHeight = 700;
+
+GLuint
+VaoId,
+VboId,
+VertexShaderId,
+FragmentShaderId,
+ProgramId;
+
+mat4	model_mat_cpp,
+view_mat_cpp,
+projection_mat_cpp;;			//Ma trận view	
+
+// Biến truyền dữ liệu cho uniform
+int		model_mat_location,
+view_mat_location,
+projection_mat_location;
+float r = 0;
+// ------------------------------------------
+string ReadShaderSourceFile(string fileName) {
+	fstream reader(fileName.c_str());
+	string line;
+	string code = "";
+	while (getline(reader, line)) {
+		code += line + "\n";
+	}
+	reader.close();
+	return code;
+}
+
+// ------------------------------------------
+void CreatVaoVbo()
+{
+	glGenVertexArrays(1, &VaoId);
+	glBindVertexArray(VaoId);
+
+	glGenBuffers(1, &VboId);
+	glBindBuffer(GL_ARRAY_BUFFER, VboId);
+	glBufferData(GL_ARRAY_BUFFER, BufferSize, Vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, VertexSize, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, VertexSize, (GLvoid*)RgbOffset);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+}
+// ------------------------------------------
+void CreatShaders()
+{
+	string vertexSrc = ReadShaderSourceFile("./vs.shader");
+	string fragmentSrc = ReadShaderSourceFile("./fs.shader");
+
+	const GLchar* VertexShader = vertexSrc.c_str();
+	const GLchar* FragmentShader = fragmentSrc.c_str();
+
+	VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+	glCompileShader(VertexShaderId);
+
+	FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+	glCompileShader(FragmentShaderId);
+
+	ProgramId = glCreateProgram();
+	glAttachShader(ProgramId, VertexShaderId);
+	glAttachShader(ProgramId, FragmentShaderId);
+	glLinkProgram(ProgramId);
+	glUseProgram(ProgramId);
+}
+// ------------------------------------------
+void CloseFunc()
+{
+	glUseProgram(0);
+
+	glDetachShader(ProgramId, VertexShaderId);
+	glDetachShader(ProgramId, FragmentShaderId);
+
+	glDeleteShader(FragmentShaderId);
+	glDeleteShader(VertexShaderId);
+
+	glDeleteProgram(ProgramId);
+
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VboId);
+
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VaoId);
+}
+// ------------------------------------------
+float tx = 0;
+void cube() {
+	mvstack.push(model_mat_cpp);
+	mat4 instance = identity_mat4();
+	mat4 model_cube = model_mat_cpp * instance;
+	glUniformMatrix4fv(model_mat_location, 1, GL_FALSE, model_cube.m);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	model_mat_cpp = mvstack.pop();
+}
+void thanhngang() {
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * scale(vec3(2, 0.25, 0.3f));
+	cube();
+	model_mat_cpp = mvstack.pop();
+}
+void thanhdoc() {
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * scale(vec3(0.3, 3, 0.3f));
+	cube();
+	model_mat_cpp = mvstack.pop();
+}
+void tamgiua() {
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * scale(vec3(1.2, 3, 0.25f)) * rotate_y(90);
+	cube();
+	model_mat_cpp = mvstack.pop();
+}
+void chotcua() {
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * scale(vec3(0.25, 0.05, 0.025)) * rotate_y(180);
+	cube();
+	model_mat_cpp = mvstack.pop();
+}
+void canhcua() {
+	mvstack.push(model_mat_cpp);
+
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * translate(vec3(0, -1.65, 0));
+	thanhngang();
+	model_mat_cpp = mvstack.pop();
+
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * translate(vec3(-0.8, 0, 0));
+	thanhdoc();
+	model_mat_cpp = mvstack.pop();
+
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * translate(vec3(0, 1.65, 0));
+	thanhngang();
+	model_mat_cpp = mvstack.pop();
+
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * translate(vec3(0.8, 0, 0));
+	thanhdoc();
+	model_mat_cpp = mvstack.pop();
+
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp;
+	tamgiua();
+	model_mat_cpp = mvstack.pop();
+
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * translate(vec3(0.75,0,0.2)) * translate(vec3(-0.1,0,0)) * rotate_z(r) * translate(vec3(0.1, 0, 0));
+	chotcua();
+	model_mat_cpp = mvstack.pop();
+
+
+	model_mat_cpp = mvstack.pop();
+}
+float xoaycua = 0;
+void DisplayFunc(void)
+{
+	model_mat_cpp = identity_mat4();
+	model_mat_location = glGetUniformLocation(ProgramId, "model_mat_shader");
+	glUniformMatrix4fv(model_mat_location, 1, GL_FALSE, model_mat_cpp.m);
+	//Các tham số của hàm lookat
+	vec3	eye(2.0f, -1.0f, 2.5f),
+		at(0.0f, 0.0f, 0.0f),
+		up(0.0f, -1.0f, 0.0f);
+
+	view_mat_cpp = lookat(eye, at, up);
+	view_mat_location = glGetUniformLocation(ProgramId, "view_mat_shader");
+	glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view_mat_cpp.m);
+
+	float	znear = 0.1f,
+		zfar = 100.0f,
+		fov = 90.0f,
+		aspect = (float)CurrentWidth / CurrentHeight;
+	projection_mat_cpp = perspective(fov, aspect, znear, zfar);
+	projection_mat_location = glGetUniformLocation(ProgramId, "projection_mat_shader");
+	glUniformMatrix4fv(projection_mat_location, 1, GL_FALSE, projection_mat_cpp.m);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	mvstack.push(model_mat_cpp);
+	model_mat_cpp = model_mat_cpp * translate(vec3(tx, 0, 0)) * rotate_y(xoaycua);
+	canhcua();
+	model_mat_cpp = mvstack.pop();
+	glutSwapBuffers();
+}
+// ------------------------------------------
+void ReshapeFunc(int Width, int Height)
+{
+	CurrentWidth = Width;
+	CurrentHeight = Height;
+
+	glViewport(0, 0, CurrentWidth, CurrentHeight);
+}
+// ------------------------------------------
+void IdleFunc(void)
+{
+	glutPostRedisplay();
+}
+// ------------------------------------------
+void KeyboardFunc(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 27:
+		glutLeaveMainLoop(); 
+		break;
+	case 't':
+		tx += 0.15;
+		break;
+	case 'p':
+		tx -= 0.15;
+		break;
+	case 'k':
+		r -= 5;
+		break;
+	case 'K':
+		r += 5;
+		break;
+	case 'm':
+		xoaycua += 30;
+		break;
+
+	}
+}
+
+// ------------------------------------------
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitContextVersion(4, 0);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInitWindowSize(CurrentWidth, CurrentHeight);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutCreateWindow("Lookat");
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	CreatVaoVbo();
+	CreatShaders();
+
+	glutDisplayFunc(DisplayFunc);
+	glutReshapeFunc(ReshapeFunc);
+	glutIdleFunc(IdleFunc);
+	glutKeyboardFunc(KeyboardFunc);
+	glutCloseFunc(CloseFunc);
+
+	glutMainLoop();
+	return 0;
+}
+// --------------------------------------
+bool CompileShader(GLuint shaderID) {
+	cout << "Compiling shader" << shaderID << "..." << endl;
+	glCompileShader(shaderID);
+	int result = -1;
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &result);
+	if (result != GL_TRUE) {
+		cout << "Shader " << shaderID << " compile failed" << endl;
+		int maxLength = 2048;
+		int realLength = 0;
+		char log[2048];
+		glGetShaderInfoLog(shaderID, maxLength, &realLength, log);
+		cout << log << endl;
+		return false;
+	}
+	cout << "Shader " << shaderID << " compile successful" << endl;
+	return true;
+}
